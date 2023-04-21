c8ffd0ff949eea3e8ed55501c3d00c2ca737a567
Francisco <gabriellimma27@gmail.com>
2023-04-21 17:12:40 -0300

    navigation to profile page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import { Container, GithubLogo, SearchForm } from './styles';
+import { Container, GithubLogo, SearchForm } from "./styles";
+import { useNavigate } from "react-router-dom";
 
 const Header: React.FC = () => {
-  return (
-    <Container>
-        <GithubLogo />
-		<SearchForm>
-			<input placeholder="Enter username of repo..." />
-		</SearchForm>
-    </Container>
-  );
-}
+	const [search, setSearch] = useState("");
+	const navigate = useNavigate();
+
+	function handleSubmit(event: React.FormEvent) {
+		event.preventDefault();
+
+		navigate("/" + search.toLowerCase().trim());
+	}
+
+	return (
+		<Container>
+			<GithubLogo />
+			<SearchForm onSubmit={handleSubmit}>
+				<input
+					placeholder="Enter username of repo..."
+					value={search}
+					onChange={(res) => setSearch(res.currentTarget.value)}
+				/>
+			</SearchForm>
+		</Container>
+	);
+};
 
 export default Header;
